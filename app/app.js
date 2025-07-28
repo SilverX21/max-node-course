8c94b86e33e875067ac19d9a697696f4f05fd497
SilverX21 <nuno21araujo95@gmail.com>
2025-07-29 00:38:20 +0100

    Refactor app.js to integrate Express and add middleware for request handling

--- app/app.js
+++ app/app.js
@@ -1,7 +1,26 @@
 const http = require("http");
 
-console.log(routes.someText);
+//here we import the express module
+const express = require("express");
 
-const server = http.createServer();
+//here we create an instance of express
+//this instance will be used to create the server and handle requests
+const app = express();
+
+//use is used to add some middleware to the express app
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); //this will call the next middleware in the stack
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+
+  //this will send a response to the client
+  //as you can see, here we don't use the next() function because the res.send() will end the request-response cycle
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
