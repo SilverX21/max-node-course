ff29f35cf0ac0e7d721926f52e128dd8a9e09498
SilverX21 <nuno21araujo95@gmail.com>
2025-12-29 18:40:42 +0000

    message update

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,6 +2,7 @@
 
 const authController = require("../controllers/auth");
 const {check, body} = require("express-validator");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -9,7 +10,12 @@ router.get("/login", authController.getLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/login", authController.postLogin);
+router.post("/login", [
+    body("email")
+        .isEmail().withMessage("Please enter a valid email"),
+    body("password", "Please enter a password with only numbers and text and at least 5 characters")
+        .isLength({min: 5}).isAlphanumeric(),
+], authController.postLogin);
 
 router.post(
     "/signup",
@@ -18,14 +24,28 @@ router.post(
             .isEmail()
             .withMessage("Please enter a valid email")
             .custom((value, {req}) => {
-                if (value === "test@test.com") {
-                    throw new Error("This email address is forbiden");
-                }
-
-                return true;
+                // if (value === "test@test.com") {
+                //     throw new Error("This email address is forbiden");
+                // }
+                //
+                // return true;
+                return User.findOne({email: value})
+                    .then(userDoc => {
+                        if (userDoc) {
+                            return Promise.reject("E-mail exists already, please pick a different one")
+                        }
+                    });
             }),
         //if a message can be applied to every validation, we can pass it in the second argument like this
-        body("password", "Please enter a password with only numbers and text and at least 5 characters").isLength({min: 5}).isAlphanumeric(),
+        body("password", "Please enter a password with only numbers and text and at least 5 characters").isLength({min: 5})
+            .isAlphanumeric(),
+        //we can check if the password and confirmPassword match like this below
+        body("confirmPassword").custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error("Passwords have to match");
+            }
+            return true;
+        })
     ],
     authController.postSignup
 );
