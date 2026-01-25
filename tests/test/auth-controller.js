f990d50be474d6952d3d409baed7277cf1210d3d
Nuno <nuno21araujo95@gmail.com>
2026-01-25 02:17:39 +0000

    tests section done

--- tests/test/auth-controller.js
+++ tests/test/auth-controller.js
@@ -1,12 +1,37 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 //the done argument is needed to avoid Mocha's timeout on async tests
 //we use the done() function to wait for async code blocks to complete
-describe("Auth Controller - Login", function (done) {
+describe("Auth Controller", function (done) {
+  //before will run once before all tests in this block
+  before(function (done) {
+    //here we connect to a test database so we don't mess with the production one
+    mongoose
+      .connect(
+        "mongodb+srv://silver21guitar02_db_user:bFYgz2b2oXSm2X4r@silvercluster.4hbkqav.mongodb.net/test-messages?appName=SilverCluster",
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "64a7f0c9f1d2c12f34567999",
+        });
+
+        return user.save();
+      })
+      .then(() => done());
+  });
+
+  //beforeEach will run before each test in this block
+  //so, if we needed to reset some state before each test, we could do it here
+
   it("should throw an error with code 500 if accessing the database fails", function () {
     sinon.stub(User, "findOne");
     User.findOne.throws();
@@ -25,5 +50,41 @@ describe("Auth Controller - Login", function (done) {
     });
 
     User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", function (done) {
+    const req = { userId: "64a7f0c9f1d2c12f34567999" };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I am new!");
+      done();
+    });
+  });
+
+  //afterEach will run after each test in this block
+  //so we could use it to restore some state after each test
+
+  //after will run once after all tests in this block
+  //here we do the cleanup by deleting the test user and disconnecting from the database
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
   });
 });
---
+++ tests/test/feed-controller.js
@@ -0,0 +1,64 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const mongoose = require("mongoose");
+
+const User = require("../models/user");
+const Post = require("../models/post");
+const FeedController = require("../controllers/feed");
+
+describe("Feed Controller", function (done) {
+  before(function (done) {
+    mongoose
+      .connect(
+        "mongodb+srv://silver21guitar02_db_user:bFYgz2b2oXSm2X4r@silvercluster.4hbkqav.mongodb.net/test-messages?appName=SilverCluster",
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "64a7f0c9f1d2c12f34567999",
+        });
+
+        return user.save();
+      })
+      .then(() => done());
+  });
+
+  it("should add a created post to the posts of the creator", function (done) {
+    const req = {
+      body: {
+        title: "Test post",
+        content: "Test post",
+      },
+      file: {
+        path: "images/test.png",
+      },
+      userId: "64a7f0c9f1d2c12f34567999",
+    };
+
+    const res = {
+      status: function () {
+        return this;
+      },
+      json: function () {},
+    };
+
+    FeedController.createPost(req, res, () => {}).then((savedUser) => {
+      expect(savedUser).to.have.property("posts");
+      expect(savedUser.posts).to.have.length(1);
+      done();
+    });
+  });
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
